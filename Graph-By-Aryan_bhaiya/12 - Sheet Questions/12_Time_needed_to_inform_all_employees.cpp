6b2135177080159ff368a81f2c5f58def2945e77
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-03-17 13:55:14 +0530

    Solved Sheet Graph section last Problem

--- Graph-By-Aryan_bhaiya/12 - Sheet Questions/12_Time_needed_to_inform_all_employees.cpp
+++ Graph-By-Aryan_bhaiya/12 - Sheet Questions/12_Time_needed_to_inform_all_employees.cpp
@@ -13,6 +13,28 @@ const long long MOD = 1e9 + 7;
 const long long INF = LLONG_MAX >> 1; 
 const long long NINF = LLONG_MIN;
 
+// In Such type of Question, time is Moving continously
+// Aap level by level soch sakte ho lekin apko time ka track rkhna honga...
+// max time kitna lga kisi bhi employee ko inform hone me ....
+// Hum employee ko as a node dikhayenge... and kisi bhi node ko max time kitna lga inform hone me uska track rkhnge...
+
+// In queue --> { node, time taken to inform }
+
+// REMEMBER --> In such type of Question, like min. no. of minutes, then think for a Clock running.. Dont think that in graph, it will be all 
+//              about height....
+// ho sakta hai Jyada depth wla child bhi Phle jo time lga tha kisi node ko inform hone me ussi time interval me visit ho gya...
+
+// EX --> n = 11, head = 4 , manager - [5,9,6,10,-1,8,9,1,9,3,4] , time - [0,213,0,253,686,170,975,0,261,309,337]
+
+// Iska tree banao, last 3rd level me 6th wla employee 2nd emp ko inform krta hai with time 975 ... 
+// ussi level me 8th wla employee 5th emp ko inform krta hai 261 me ... 5th emp jldi inform hua --> Now it will inform 0th emp at +170 minutes..
+// 8th --> 5th ---> 0th  =  0 + 261 + 170 = 430 min , see 8 -> 0 tk visit ho gya 430 min me .. But note that ki 6th --> 2nd wla emp abhi bhi 
+// inform nhi hua --> 975 - 430 == 545 min is remaining.... 
+// Max time for a employee to get informed ---> 965 [ just taking this case from that level ]
+
+// Therefore har ek node ka track rkhna honga unke time ke sath....
+
+
 int numOfMinutes(int n, int head, vector<int>& arr, vector<int>& time) {
 
     vector<int> gr[n];
@@ -25,38 +47,32 @@ int numOfMinutes(int n, int head, vector<int>& arr, vector<int>& time) {
 
     int ans = 0;
 
-    queue<int> q;
-    q.push(head);
-    vector<int> vis(n, 0);
-    vis[head] = 0;
+    queue<pair<int,int>> q;
+    q.push({head, 0});
 
     while(!q.empty()){
 
-        int z = q.size();
-        int mx = 0;
+        int id = q.front().first;
+        int t = q.front().second;
+        q.pop();
 
-        for(int i=0;i<z;i++){
+        ans = max(ans, t);
 
-            int id = q.front();
-            q.pop();
+        for(int x : gr[id]){
 
-            mx = max(mx, time[id]);
-
-            for(int x : gr[id]){
-
-                if(vis[x] == 0){
-                    vis[x] = 1;
-                    q.push(x);
-                }
-
-            }
+            q.push({x, t + time[id]});
 
         }
 
-        ans += mx;
+
 
     }
 
     return ans;
         
 }
+
+// DFS --->
+// jha pe vis[] = true krte ho wha pe max lena to update the mx
+// Keep a running sum --> 
+// in for loop --> phle running sum me add kro inform time curr src ka and call ke baad changes backtrack kro i.e running sum se - inform time
--- Graph-By-Aryan_bhaiya/12 - Sheet Questions/new.cpp
+++ Graph-By-Aryan_bhaiya/12 - Sheet Questions/new.cpp
@@ -13,25 +13,3 @@ const long long MOD = 1e9 + 7;
 const long long INF = LLONG_MAX >> 1;
 const long long NINF = LLONG_MIN;
 
-int firstUniqChar(string s) {
-
-    int n = s.size();
-
-    int cnt[256] = {0};
-
-    int indx = -1;
-
-    for(int i=n-1;i>=0;i--){
-
-        if(cnt[s[i]] == 0){
-            indx = i;
-        }
-        cnt[s[i]]++;
-
-    }
-
-    if(indx != -1 && cnt[s[indx]] == 1) return indx;
-
-    return indx; 
-        
-}
